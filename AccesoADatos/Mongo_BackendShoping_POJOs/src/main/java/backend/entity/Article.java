004643e0108c52114aab70c209244bcfa9068d83
Aspen Carsi <159141789+enbeetreee@users.noreply.github.com>
2026-09-20 16:07:00 +0200

    Add files via upload

---
+++ AccesoADatos/Mongo_BackendShoping_POJOs/src/main/java/backend/entity/Article.java
@@ -0,0 +1,65 @@
+package backend.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+public class Article {
+	private ObjectId id;
+
+	private String name;
+	private double price;
+	private List<String> categories;
+	private List<Comment> comments = new ArrayList<Comment>();
+	public Article() {
+		super();
+		id = new ObjectId();
+	}
+	public Article(String name, double price, List<String> categories) {
+		this();
+
+		this.name = name;
+		this.price = price;
+		this.categories = categories;
+	}
+	
+	public ObjectId getId() {
+		return id;
+	}
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public List<String> getCategories() {
+		return categories;
+	}
+	public void setCategories(List<String> categories) {
+		this.categories = categories;
+	}
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", name=" + name + ", price=" + price + ", categories=" + categories
+				+ ", comments=" + comments + "]";
+	}
+	
+	
+}
